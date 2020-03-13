74729c58aebf451cc43c82691b3e0465e241b8b2
孙振兴 <44517244+Sun-ZhenXing@users.noreply.github.com>
2020-03-13 14:11:20 +0800

    Update core.py

--- visualmath/core.py
+++ visualmath/core.py
@@ -6,23 +6,22 @@
 from .utils import Functional_Method
 from .errorHandler import handler_main
 from traceback import print_exc
-from eventlet import Timeout,monkey_patch
+# from eventlet import Timeout,monkey_patch
 
-TimeLimit = 2
+# TimeLimit = 2
 # 每次请求的计算时间，VIP特殊服务还没写
 DebugMode = True
 # 调试模式
 
-monkey_patch()
 print("[Msg] Visual Math loading...")
 def compute(content, setting="default"):
     try:
-        with Timeout(TimeLimit, TimeoutError("Timeout. VIP can compute 25s per time.")):
-            jud = content.split(" ")[0]
-            if jud in Functional_Method:
-                output = Functional_Method[jud][0](jud, content)
-            else:
-                output = Functional_Method["default"][0](content)
+        # with Timeout(TimeLimit, TimeoutError("Timeout. VIP can compute 25s per time.")):
+        jud = content.split(" ")[0]
+        if jud in Functional_Method:
+            output = Functional_Method[jud][0](jud, content)
+        else:
+            output = Functional_Method["default"][0](content)
     except Exception as error:
         output = handler_main(error)
         if DebugMode:
